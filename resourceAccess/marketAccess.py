0b3e6cfa2a1b064031fe36fb42b98317b5789269
Anup Chandak <85943077+anup-chandak@users.noreply.github.com>
2025-07-02 23:26:33 +0530

    Update marketAccess.py

--- resourceAccess/marketAccess.py
+++ resourceAccess/marketAccess.py
@@ -1,12 +1,20 @@
-# resourceAccess/marketAccess.py
+
+from entities.market import Market
 from resources.MarketDb import MarketDb
 
 class MarketAccess:
     def __init__(self, market_db: MarketDb):
         self.db = market_db
 
-    def get_current_price(self):
+    def get_market(self) -> Market:
+        price = self.db.ReadPrice()
+        return Market(init_price=price)
+
+    def save_market(self, market: Market) -> None:
+        self.db.WritePrice(market.current_price)
+
+    def get_current_price(self) -> float:
         return self.db.ReadPrice()
 
-    def set_price(self, new_price):
+    def set_price(self, new_price: float) -> None:
         self.db.WritePrice(new_price)
