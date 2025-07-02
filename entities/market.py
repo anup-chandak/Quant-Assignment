9792be2a1f92ea977b6cbba73e2bae5556e49272
Anup Chandak <85943077+anup-chandak@users.noreply.github.com>
2025-07-02 23:27:37 +0530

    Update market.py

--- entities/market.py
+++ entities/market.py
@@ -4,6 +4,7 @@ class Market:
         self.current_price = init_price
         self.price_history: list[float] = [init_price]
 
-    def update_price(self, new_price: float):
+    def update_price(self, net_demand: int, impact: float = 0.2):
+        new_price = max(0.01, self.current_price + impact * net_demand)
         self.current_price = new_price
         self.price_history.append(new_price)
