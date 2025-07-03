325e95dfd598b1bbb49abe22be0c16fbd7ec353e
Anup Chandak <85943077+anup-chandak@users.noreply.github.com>
2025-07-03 10:14:20 +0530

    Update trader.py

--- entities/trader.py
+++ entities/trader.py
@@ -1,5 +1,3 @@
-
-
 class Trader:
     def __init__(self, id: int, cash: float, inventory: float):
         self.id = id
@@ -7,7 +5,7 @@ class Trader:
         self.inventory = inventory
         self.net_worth_history: list[float] = []
 
-    def buy(self, volume: int, price: float) -> bool:
+    def try_buy(self, volume: int, price: float) -> bool:
         cost = volume * price
         if cost > self.cash:
             return False
@@ -15,7 +13,7 @@ class Trader:
         self.inventory += volume
         return True
 
-    def sell(self, volume: int, price: float) -> bool:
+    def try_sell(self, volume: int, price: float) -> bool:
         if volume > self.inventory:
             return False
         self.inventory -= volume
